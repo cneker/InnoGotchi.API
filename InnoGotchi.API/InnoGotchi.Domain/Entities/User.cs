0cbef5b2947c23c3ad8eff543897fa75ee2fb05f
cneker <staskotashevich@gmail.com>
2023-01-18 21:03:02 +0300

    changed User PasswordHash field type to string, removed FramId field

--- InnoGotchi.API/InnoGotchi.Domain/Entities/User.cs
+++ InnoGotchi.API/InnoGotchi.Domain/Entities/User.cs
@@ -7,8 +7,7 @@
         public string LastName { get; set; }
         public string AvatarPath { get; set; }
         public string Email { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public Guid FarmId { get; set; }
+        public string PasswordHash { get; set; }
 
         public Farm UserFarm { get; set; }
         public ICollection<Farm> FriendsFarm { get; set; }
