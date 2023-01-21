4255a77f835816c2da0c738b1e6fda60fbb1e26a
cneker <staskotashevich@gmail.com>
2023-01-21 13:43:18 +0300

    added mappings Farm -> FarmStatisticsDto, Pet -> PetOverviewDto

--- InnoGotchi.API/InnoGotchi.Application/Mapper/MappingProfile.cs
+++ InnoGotchi.API/InnoGotchi.Application/Mapper/MappingProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using InnoGotchi.Application.Contracts.Services;
 using InnoGotchi.Application.DataTransferObjects.Farm;
+using InnoGotchi.Application.DataTransferObjects.Pet;
 using InnoGotchi.Application.DataTransferObjects.User;
 using InnoGotchi.Domain.Entities;
 
@@ -7,13 +9,20 @@ namespace InnoGotchi.Application.Mapper
 {
     public class MappingProfile : Profile
     {
-        public MappingProfile()
+        public MappingProfile(ICalculatePetAgeService calculatePetAgeService)
         {
             CreateMap<UserForRegistrationDto, User>();
             CreateMap<UserInfoDto, User>();
 
             CreateMap<Farm, FarmOverviewDto>();
             CreateMap<Farm, FarmDetailsDto>();
+            CreateMap<Farm, FarmStatisticsDto>();
+
+            CreateMap<Pet, PetOverviewDto>()
+                .ForMember(p => p.Age, 
+                    opt => opt.MapFrom(p => calculatePetAgeService.CalculateAge(p.Birthday, p.DeathDay)))
+                .ForMember(p => p.IsAlive, 
+                    opt => opt.MapFrom(p => p.DeathDay == null));
         }
     }
 }
