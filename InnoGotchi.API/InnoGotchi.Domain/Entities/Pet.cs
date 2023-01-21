45fddf840b19ef051c8af842ca18483cb7df96d5
cneker <staskotashevich@gmail.com>
2023-01-21 13:30:46 +0300

    some changes in Farm entity

--- InnoGotchi.API/InnoGotchi.Domain/Entities/Pet.cs
+++ InnoGotchi.API/InnoGotchi.Domain/Entities/Pet.cs
@@ -7,17 +7,19 @@ namespace InnoGotchi.Domain.Entities
         public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime Birthday { get; set; }
-        public DateTime DeathDay { get; set; }
+        public DateTime? DeathDay { get; set; }
         public int HappynessDayCount { get; set; }
         public HungerLevel HungerLevel { get; set; }
         public ThirstyLevel ThirstyLevel { get; set; }
         public DateTime LastFed { get; set; }
         public DateTime LastDrank { get; set; }
+        public double FeedingPeriod { get; set; }
+        public double ThirstQuenchingPeriod { get; set; }
         public Body Body { get; set; }
         public Eye Eye { get; set; }
         public Nose Nose { get; set; }
         public Mouth Mouth { get; set; }
-        public Guid FramId { get; set; }
+        public Guid FarmId { get; set; }
 
         public Farm Farm { get; set; }
     }
