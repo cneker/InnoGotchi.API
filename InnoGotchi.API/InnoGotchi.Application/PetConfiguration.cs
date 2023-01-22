49cacfb563fcd1f6fe643811f267e78f674aa6d2
cneker <staskotashevich@gmail.com>
2023-01-22 21:10:03 +0300

    added pet configuration class

---
+++ InnoGotchi.API/InnoGotchi.Application/PetConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace InnoGotchi.Application
+{
+    public static class PetConfiguration
+    {
+        public static readonly int OnePetAgeInDays = 7;
+        public static readonly int FeedingFrequencyInHours = 6;
+        public static readonly int DrinkingFrequencyInHours = 3;
+
+    }
+}
