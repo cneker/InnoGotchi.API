6e5b7e79eb5730a65c75f603cf148257ade64147
cneker <staskotashevich@gmail.com>
2023-01-23 19:44:37 +0300

    registered all services; added basic configuration to API

---
+++ InnoGotchi.API/InnoGotchi.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Infrastructure.Persistance;
+using Infrastructure.Persistance.Repositories;
+using Infrastructure.Services;
+using InnoGotchi.Application.Contracts.Repositories;
+using InnoGotchi.Application.Contracts.Services;
+using InnoGotchi.Application.Mapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace InnoGotchi.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services,
+            IConfiguration configuration) =>
+                services.AddDbContext<AppDbContext>(opts =>
+                    opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b =>
+                        b.MigrationsAssembly("Infrastructure.Persistance")));
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+        public static void ConfigureAutoMapper(this IServiceCollection services) =>
+            services.AddScoped(provider => new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile(provider.GetService<IPetConditionService>()));
+            }).CreateMapper());
+
+        public static void ConfigureAuthenticationService(this IServiceCollection services) =>
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+        public static void ConfigureFarmService(this IServiceCollection services) =>
+            services.AddScoped<IFarmService, FarmService>();
+
+        public static void ConfigureGenerateFarmStatisticsService(this IServiceCollection services) =>
+            services.AddScoped<IGenerateFarmStatisticsService, GenerateFarmStatisticsService>();
+
+        public static void ConfigureGenerateTokenService(this IServiceCollection services) =>
+            services.AddScoped<IGenerateTokenService, GenerateTokenService>();
+
+        public static void ConfigurePetConditionService(this IServiceCollection services) =>
+            services.AddScoped<IPetConditionService, PetConditionService>();
+
+        public static void ConfigurePetService(this IServiceCollection service) =>
+            service.AddScoped<IPetService, PetService>();
+
+        public static void ConfigureUserService(this IServiceCollection services) =>
+            services.AddScoped<IUserService, UserService>();
+    }
+}
--- InnoGotchi.API/InnoGotchi.API/Program.cs
+++ InnoGotchi.API/InnoGotchi.API/Program.cs
@@ -1,24 +1,42 @@
+using FluentValidation;
+using InnoGotchi.API.Extensions;
+using InnoGotchi.Application.Validators.User;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureAuthenticationService();
+builder.Services.ConfigureFarmService();
+builder.Services.ConfigureGenerateFarmStatisticsService();
+builder.Services.ConfigureGenerateTokenService();
+builder.Services.ConfigurePetConditionService();
+builder.Services.ConfigurePetService();
+builder.Services.ConfigureUserService();
+builder.Services.ConfigureAutoMapper();
+builder.Services.AddValidatorsFromAssemblyContaining<UserForRegistrationDtoValidator>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(opt =>
+{
+    opt.SuppressAsyncSuffixInActionNames = false;
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseRouting();
+//app.UseAuthorization();
 
 app.MapControllers();
 
