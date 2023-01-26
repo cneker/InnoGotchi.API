c2ac6b154d6a5ac72ed47c2bd42e6a75829d1665
cneker <staskotashevich@gmail.com>
2023-01-26 15:51:16 +0300

    added MappingTests

---
+++ InnoGotchi.API/Tests/Mapping/MappingTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using InnoGotchi.Application.Contracts.Services;
+using InnoGotchi.Application.Mapper;
+using InnoGotchi.Domain.Entities;
+
+namespace Tests.Mapping
+{
+    public class MappingTests
+    {
+        [Fact]
+        public void TestingMappings()
+        {
+            //Arrange
+            var serviceMock = new Mock<IPetConditionService>();
+            serviceMock.Setup(a => a.IsPetAlive(It.IsAny<Pet>()))
+                .Returns(It.IsAny<bool>());
+            serviceMock.Setup(a => a.CalculateAge(It.IsAny<Pet>()))
+                .Returns(It.IsAny<int>());
+            serviceMock.Setup(a => a.UpdatePetsFeedingAndDrinkingLevelsByFarm(It.IsAny<Guid>()))
+                .Returns(Task.CompletedTask);
+            var mapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile(serviceMock.Object));
+            }).CreateMapper();
+
+            //Act and Assert
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
