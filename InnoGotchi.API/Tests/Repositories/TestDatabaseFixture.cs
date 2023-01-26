7a1e87d9ea7f8c44d6ab5ce95439c7f4a6d4f716
cneker <staskotashevich@gmail.com>
2023-01-26 16:01:18 +0300

    added UserRepositoryTests

---
+++ InnoGotchi.API/Tests/Repositories/TestDatabaseFixture.cs
@@ -0,0 +1,88 @@
+﻿using Infrastructure.Persistance;
+using InnoGotchi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tests.Repositories
+{
+    public class TestDatabaseFixture
+    {
+        private readonly string _connectionString 
+            = "server=(localdb)\\mssqllocaldb; database=InnoGotchiTests; Integrated Security = true";
+        private readonly Fixture _fixture;
+        public AppDbContext Context { get; init; }
+
+        private AppDbContext CreateContext() =>
+            new AppDbContext(
+                new DbContextOptionsBuilder<AppDbContext>()
+                    .UseSqlServer(_connectionString)
+                    .Options);
+
+        public TestDatabaseFixture()
+        {
+            _fixture = new Fixture();
+            Context = CreateContext();
+            Context.Database.EnsureCreated();
+            Cleanup();
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var users = new List<User>
+            {
+                _fixture.Build<User>()
+                    .With(u => u.Id, Guid.Parse("8cd6c906-81c5-4717-8428-bfebe34d0f09"))
+                    .With(u => u.Email, "email1@mail.com")
+                    .Without(u => u.FriendsFarms)
+                    .Without(u => u.UserFarm)
+                    .Create(),
+                _fixture.Build<User>()
+                    .Without(u => u.FriendsFarms)
+                    .Without(u => u.UserFarm)
+                    .Create(),
+                _fixture.Build<User>()
+                    .With(u => u.Email, "email2@mail.com")
+                    .Without(u => u.FriendsFarms)
+                    .Without(u => u.UserFarm)
+                    .Create()
+            };
+
+            Context.Users.AddRange(users);
+            Context.SaveChanges();
+
+            var farms = new List<Farm>
+            {
+                _fixture.Build<Farm>()
+                    .With(f => f.UserId, users[0].Id)
+                    .With(f => f.Collaborators, new List<User>())
+                    .Without(f => f.User)
+                    .Without(f => f.Pets)
+                    .Create(),
+                _fixture.Build<Farm>()
+                    .With(f => f.UserId, users[1].Id)
+                    .With(f => f.Collaborators, new List<User>())
+                    .Without(f => f.User)
+                    .Without(f => f.Pets)
+                    .Create(),
+            };
+
+            Context.Farms.AddRange(farms);
+            Context.SaveChanges();
+
+            farms[0].Collaborators.Add(users[1]);
+            farms[0].Collaborators.Add(users[2]);
+
+            farms[1].Collaborators.Add(users[0]);
+
+            Context.SaveChanges();
+        }
+
+        private void Cleanup()
+        {
+            Context.Farms.RemoveRange(Context.Farms);
+            Context.Users.RemoveRange(Context.Users);
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ InnoGotchi.API/Tests/Repositories/UserRepositoryTests.cs
@@ -0,0 +1,130 @@
+﻿using Infrastructure.Persistance;
+using Infrastructure.Persistance.Repositories;
+using InnoGotchi.Domain.Entities;
+
+namespace Tests.Repositories
+{
+    public class UserRepositoryTests : IClassFixture<TestDatabaseFixture>
+    {
+        private readonly Fixture _fixture;
+        private readonly AppDbContext _context;
+        private readonly UserRepository _repository;
+        public UserRepositoryTests(TestDatabaseFixture fixture)
+        {
+            _fixture = new Fixture();
+            _context = fixture.Context;
+            _repository = new UserRepository(_context);
+        }
+
+        [Fact]
+        public async Task GetUsersAsync_ReturnsUsersList()
+        {
+            //Arrange
+
+            //Act
+            var result = await _repository.GetUsersAsync(false);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().HaveCount(3);
+        }
+
+        [Fact]
+        public async Task GetUserByIdAsync_ReturnsUser_WhenUserExistsAndPassingIdIsValid()
+        {
+            //Arrange
+            var userId = Guid.Parse("8cd6c906-81c5-4717-8428-bfebe34d0f09");
+
+            //Act
+            var result = await _repository.GetUserByIdAsync(userId, false);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<User>();
+            result.Id.Should().Be(userId);
+            result.UserFarm.Should().NotBeNull();
+            result.FriendsFarms.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetUserByIdAsync_ReturnsNull_WhenPassedIdIsInvalid()
+        {
+            //Arrange
+            var userId = _fixture.Create<Guid>();
+
+            //Act
+            var result = await _repository.GetUserByIdAsync(userId, false);
+
+            //Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetUserByEmailAsync_ReturnsUser_WhenUserExistsAndPassingEmailIsValid()
+        {
+            //Arrange
+            var userEmail = "email1@mail.com";
+
+            //Act
+            var result = await _repository.GetUserByEmailAsync(userEmail, false);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<User>();
+            result.Email.Should().Be(userEmail);
+            result.UserFarm.Should().NotBeNull();
+            result.FriendsFarms.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetUserByEmailAsync_ReturnsNull_WhenPassedEmailIsInvalid()
+        {
+            //Arrange
+            var userEmail = _fixture.Create<string>();
+
+            //Act
+            var result = await _repository.GetUserByEmailAsync(userEmail, false);
+
+            //Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task CreateUser_SavedUser()
+        {
+            //Arrange
+            var user = _fixture.Build<User>()
+                .Without(u => u.UserFarm)
+                .Without(u => u.FriendsFarms)
+                .Create();
+            _context.Database.BeginTransaction();
+
+            //Act
+            await _repository.CreateUser(user);
+            _context.SaveChanges();
+
+            //Assert
+            var result = await _repository.GetUserByIdAsync(user.Id, false);
+            _context.Database.RollbackTransaction();
+            result.Should().NotBeNull();
+            result.Id.Should().Be(user.Id);
+        }
+
+        [Fact]
+        public async Task DeleteUser_DeletedUser()
+        {
+            //Arrange
+            var user = _context.Users.Where(u => u.Email == "email2@mail.com").Single();
+            _context.Database.BeginTransaction();
+
+            //Act
+            _repository.DeleteUser(user);
+            _context.SaveChanges(true);
+
+            //Assert
+            var result = await _repository.GetUserByIdAsync(user.Id, false);
+            _context.Database.RollbackTransaction();
+            result.Should().BeNull();
+        }
+    }
+}
