c4d517a85596abb4aaf2b44bf5fb8efd4ad7f62c
cneker <staskotashevich@gmail.com>
2023-01-27 12:51:03 +0300

    added exception handler middleware

---
+++ InnoGotchi.API/InnoGotchi.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using InnoGotchi.Application.Exceptions;
+using System.Text.Json;
+
+namespace InnoGotchi.API.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                response.StatusCode = ex switch
+                {
+                    NotFoundException => StatusCodes.Status404NotFound,
+                    AlreadyExistsException => StatusCodes.Status400BadRequest,
+                    PetIsDeadException => StatusCodes.Status400BadRequest,
+                    _ => StatusCodes.Status500InternalServerError
+                };
+                var message = JsonSerializer.Serialize(new { message = ex.Message });
+
+                await response.WriteAsync(message);
+            }
+        }
+    }
+}
