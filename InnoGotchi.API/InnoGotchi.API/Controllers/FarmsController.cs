0a2cd992290727ff5321de1f43fd06aa3ee5f053
cneker <staskotashevich@gmail.com>
2023-01-30 14:33:26 +0300

    added FarmsController and PetsController

---
+++ InnoGotchi.API/InnoGotchi.API/Controllers/FarmsController.cs
@@ -0,0 +1,93 @@
+﻿using InnoGotchi.API.Filters.ActionFilters;
+using InnoGotchi.Application.Contracts.Services;
+using InnoGotchi.Application.DataTransferObjects.Farm;
+using InnoGotchi.Application.DataTransferObjects.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InnoGotchi.API.Controllers
+{
+    [Route("api/users/{userId}/farms")]
+    [ApiController]
+    public class FarmsController : ControllerBase
+    {
+        private readonly IFarmService _farmService;
+        public FarmsController(IFarmService farmService)
+        {
+            _farmService = farmService;
+        }
+
+        [HttpGet("/api/farms")]
+        public async Task<IActionResult> GetFarms()
+        {
+            var farmsDto = await _farmService.GetFarmsOverviewAsync();
+
+            return Ok(farmsDto);
+        }
+
+        [HttpGet(Name = "GetFarmOverview")]
+        public async Task<IActionResult> GetFarmOverview(Guid userId)
+        {
+            var farmDto = await _farmService.GetFarmOverviewByIdAsync(userId);
+
+            return Ok(farmDto);
+        }
+
+        [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> CreateFarm(Guid userId, [FromBody] FarmForCreationDto farmDto)
+        {
+            var farmId = await _farmService.CreateFarmAsync(userId, farmDto);
+
+            return CreatedAtRoute("GetFarmOverview", new { userId = userId }, farmId);
+        }
+
+        [HttpGet("detail")]
+        public async Task<IActionResult> GetFarmDetails(Guid userId)
+        {
+            var farmDto = await _farmService.GetFarmDetailsByIdAsync(userId);
+
+            return Ok(farmDto);
+        }
+
+        [HttpGet("statistics")]
+        public async Task<IActionResult> GetFarmStatistics(Guid userId)
+        {
+            var farmDto = await _farmService.GetFarmStatisticsByIdAsync(userId);
+
+            return Ok(farmDto);
+        }
+
+        [HttpGet("friends")]
+        public async Task<IActionResult> GetFriendsFarms(Guid userId)
+        {
+            var friends = await _farmService.GetFriendsFarmsAsync(userId);
+
+            return Ok(friends);
+        }
+
+        [HttpPost("collaborators")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> InviteCollaborator(Guid userId, [FromBody] UserForInvitingDto userDto)
+        {
+            await _farmService.InviteFriendAsync(userId, userDto);
+            return NoContent();
+        }
+
+        [HttpPut]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> UpdateFarm(Guid userId, [FromBody] FarmForUpdateDto farmDto)
+        {
+            await _farmService.UpdateFarmNameAsync(userId, farmDto);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteFarm(Guid userId)
+        {
+            await _farmService.DeleteFarmById(userId);
+
+            return NoContent();
+        }
+    }
+}
---
+++ InnoGotchi.API/InnoGotchi.API/Controllers/PetsController.cs
@@ -0,0 +1,68 @@
+﻿using InnoGotchi.API.Filters.ActionFilters;
+using InnoGotchi.Application.Contracts.Services;
+using InnoGotchi.Application.DataTransferObjects.Pet;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InnoGotchi.API.Controllers
+{
+    [Route("api/farms/{farmId}/pets")]
+    [ApiController]
+    public class PetsController : ControllerBase
+    {
+        private readonly IPetService _petService;
+        public PetsController(IPetService petService)
+        {
+            _petService = petService;
+        }
+
+        [HttpGet("/api/pets")]
+        public async Task<IActionResult> GetPets()
+        {
+            var pets = await _petService.GetAllPetsAsync();
+
+            return Ok(pets);
+        }
+
+        [HttpGet("{id}", Name = "GetPet")]
+        public async Task<IActionResult> GetPet(Guid farmId, Guid id)
+        {
+            var pet = await _petService.GetPetByIdAsync(farmId, id);
+
+            return Ok(pet);
+        }
+
+        [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> CreatePet(Guid farmId, [FromBody] PetForCreationDto petDto)
+        {
+            var petId = await _petService.CreatePetAsync(farmId, petDto);
+
+            return CreatedAtRoute("GetPet", new { farmId, id = petId }, petId);
+        }
+
+        [HttpPut("{id}")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> UpdatePetName(Guid farmId, Guid id, [FromBody] PetForUpdateDto petDto)
+        {
+            await _petService.UpdatePetNameAsync(farmId, id, petDto);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}/feed")]
+        public async Task<IActionResult> FeedThePet(Guid farmId, Guid id)
+        {
+            await _petService.FeedThePetAsync(farmId, id);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}/give-a-drink")]
+        public async Task<IActionResult> GiveADrinkToThePet(Guid farmId, Guid id)
+        {
+            await _petService.GiveADrinkToPetAsync(farmId, id);
+
+            return NoContent();
+        }
+    }
+}
