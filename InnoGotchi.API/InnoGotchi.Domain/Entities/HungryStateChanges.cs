e5ec1e586de86845b7ee3640bbadffb45141b9b2
cneker <staskotashevich@gmail.com>
2023-02-01 10:26:49 +0300

    added current thirsty/hungry state in Thirsty/HungryStateChanges

--- InnoGotchi.API/InnoGotchi.Domain/Entities/HungryStateChanges.cs
+++ InnoGotchi.API/InnoGotchi.Domain/Entities/HungryStateChanges.cs
@@ -1,10 +1,13 @@
-﻿namespace InnoGotchi.Domain.Entities
+﻿using InnoGotchi.Domain.Enums;
+
+namespace InnoGotchi.Domain.Entities
 {
     public class HungryStateChanges
     {
         public Guid Id { get; set; }
         public DateTime ChangesDate { get; set; }
         public bool IsFeeding { get; set; }
+        public HungerLevel HungerState { get; set; }
         public Guid PetId { get; set; }
 
         public Pet Pet { get; set; }
--- InnoGotchi.API/InnoGotchi.Domain/Entities/ThirstyStateChanges.cs
+++ InnoGotchi.API/InnoGotchi.Domain/Entities/ThirstyStateChanges.cs
@@ -1,10 +1,13 @@
-﻿namespace InnoGotchi.Domain.Entities
+﻿using InnoGotchi.Domain.Enums;
+
+namespace InnoGotchi.Domain.Entities
 {
     public class ThirstyStateChanges
     {
         public Guid Id { get; set; }
         public DateTime ChangesDate { get; set; }
         public bool IsDrinking { get; set; }
+        public ThirstyLevel ThirstyState { get; set; }
         public Guid PetId { get; set; }
 
         public Pet Pet { get; set; }
