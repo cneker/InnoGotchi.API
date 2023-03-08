c3cb58225c2c8c75eeb2c71c16c10cc918aa8d2a
cneker <staskotashevich@gmail.com>
2023-03-08 21:45:41 +0300

    added AvatarChangingDtoValidator and updated PasswordChangingDtoValidator

--- InnoGotchi.API/InnoGotchi.Application/Validators/User/PasswordChangingDtoValidator.cs
+++ InnoGotchi.API/InnoGotchi.Application/Validators/User/PasswordChangingDtoValidator.cs
@@ -12,6 +12,9 @@ namespace InnoGotchi.Application.Validators.User
                 .Length(5, 30)
                 .Equal(p => p.ConfirmedPassword)
                 .NotEqual(p => p.OldPassword);
+            RuleFor(p => p.ConfirmedPassword)
+                .NotEmpty()
+                .Length(5, 30);
             RuleFor(p => p.OldPassword)
                 .NotEmpty()
                 .Length(5, 30);
